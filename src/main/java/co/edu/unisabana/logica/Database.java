cd5a09e054ecf248c4cce395cfab76ef85068c76
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2026-08-13 20:06:34 -0500

    Fix code

--- src/main/java/co/edu/unisabana/logica/Database.java
+++ src/main/java/co/edu/unisabana/logica/Database.java
@@ -3,6 +3,6 @@ package co.edu.unisabana.logica;
 public class Database {
 
     public void guardarTransaccion(int monto, String tarjeta) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
     }
 }
